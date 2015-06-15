85d29291babea04d44573eb5337ad8dd620aef58
dex8D <tehdex8D@gmail.com>
2015-06-15 17:54:18 -0500

    project created
    created project

---
+++ UE_GWNS/Source/UE_GWNS/George.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE_GWNS.h"
+#include "George.h"
+
+
+// Sets default values
+AGeorge::AGeorge()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGeorge::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGeorge::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void AGeorge::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
---
+++ UE_GWNS/Source/UE_GWNS/UE_GWNS.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE_GWNS.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_GWNS, "UE_GWNS" );
